f8385e861d455706d7fbda6a60b7f7231c3b6f30
student <you@example.com>
2019-01-17 16:17:58 +0530

    lab2 tower of hanoi

---
+++ daa/labs/lab2/peak.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int i, j;
+    vector<int> array = {2, 3, 5, 7, 36, 43, 9, 1, 0, -4, -11};
+    findPeak(array, 0, array.length);
+
+}
